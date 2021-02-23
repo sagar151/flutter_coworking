88c5980941b594c0e4b320cdd6140b3cc4f19b77
sagar gajera <46403954+sagar151@users.noreply.github.com>
2021-02-23 11:03:18 +0530

    Add files via upload

---
+++ hello.dart
@@ -0,0 +1,16 @@
+//import 'dart:math';
+//import 'dart:io';
+
+import 'dart:ffi';
+
+void main() {
+  // int s;
+  //s = 20;
+  printCity("mumbai", "pune", n3: "sagar");
+}
+
+void printCity(String n1, String n2, {String n3}) {
+  print("city $n1");
+  print("city $n2");
+  print("city $n3");
+}
